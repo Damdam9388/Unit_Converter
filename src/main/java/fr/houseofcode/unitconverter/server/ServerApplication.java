f70c6d99f0ebc02e52d8d205581b3b87453dcd51
dte <dte@DT-P-003.cnam-lyon.peda>
2020-01-16 16:59:41 +0100

    Dépendances installées et projet configuré.

--- src/main/java/fr/houseofcode/unitconverter/server/ServerApplication.java
+++ src/main/java/fr/houseofcode/unitconverter/server/ServerApplication.java
@@ -2,12 +2,15 @@ package fr.houseofcode.unitconverter.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ServerApplication {
+	// String home() {
+	// return "helooworld";
+	// }
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ServerApplication.class, args);
+		System.out.println("hiFromLyon");
 	}
 
 }
