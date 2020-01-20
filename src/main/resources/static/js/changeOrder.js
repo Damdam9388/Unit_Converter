e7f7e03d9f116ee7d7ffa675c71d1b43dcdbe7ff
mre <tubingen050923>
2020-01-20 15:29:55 +0100

    #1 bouton submit + ajout parametre d'envoi des donnees du form

---
+++ src/main/resources/static/js/changeOrder.js
@@ -0,0 +1,7 @@
+function change(){
+    var firstUnit = document.getElementById("inputState");
+    var secondUnit = document.getElementById("outputState");
+
+    console.log(firstUnit);
+    console.log(secondUnit);
+}
