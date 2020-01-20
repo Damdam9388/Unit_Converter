3a0241542cdcf468bb44ca941bd923edfee48ca5
mre <tubingen050923>
2020-01-20 14:21:59 +0100

    #1 creation front accueil navbar inputs

--- src/main/java/fr/houseofcode/unitconverter/ServerApplication.java
+++ src/main/java/fr/houseofcode/unitconverter/ServerApplication.java
@@ -1,11 +1,10 @@
-package fr.houseofcode.unitconverter.server;
+package fr.houseofcode.unitconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"fr.houseofcode.unitconverter.controller"})
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
