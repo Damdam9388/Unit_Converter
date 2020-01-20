d24c6338ad8625c1d07e6f2d3840e79dd11ac2d0
karam.findakly <karam.findakly@LAPTOP-LA6A884S.lyon.sistema-strategy.com>
2020-01-20 14:01:57 +0100

    User Story 1: m2 en hectare
    En dure dans le code

---
+++ src/main/java/fr/houseofcode/unitconverter/controller/Controller.java
@@ -0,0 +1,33 @@
+package fr.houseofcode.unitconverter.controller;
+
+import javax.measure.UnitConverter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.houseofcode.unitconverter.entity.AdditionalUnits;
+import si.uom.SI;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public void main(String[] args) {
+
+        UnitConverter m2 = SI.SQUARE_METRE.getConverterTo(AdditionalUnits.HECTARE);
+        UnitConverter ha = AdditionalUnits.HECTARE.getConverterTo(SI.SQUARE_METRE);
+        double value = m2.convert(4000);
+
+        //System.out.println(m2); //not working with exception
+        //System.out.println(hectare); //not working with exception
+        //System.out.println(toto);
+        System.out.println(m2);
+        System.out.println(value);
+
+        //System.out.println(m2.divide(hectare));//not working with exception
+        //System.out.println(m2.divide(hectare).getValue());
+        //System.out.println(m2.divide(hectare).getUnit());
+
+    }
+
+}
--- src/main/java/fr/houseofcode/unitconverter/entity/AdditionalUnits.java
+++ src/main/java/fr/houseofcode/unitconverter/entity/AdditionalUnits.java
@@ -3,23 +3,55 @@
  */
 package fr.houseofcode.unitconverter.entity;
 
+import javax.measure.Unit;
+import javax.measure.quantity.Area;
+
+import tec.uom.se.AbstractUnit;
+import tec.uom.se.function.RationalConverter;
+import tec.uom.se.unit.TransformedUnit;
 import tec.uom.se.unit.Units;
 
 /**
  * @author karam.findakly
  *
  */
-public class AdditionalUnits extends Unit {
+public class AdditionalUnits extends Units {
 
     protected AdditionalUnits() {
     }
 
+    private static final AdditionalUnits INSTANCE = new AdditionalUnits();
+
     @Override
     public String getName() {
-        return "Unit Converter Specific";
+        return AdditionalUnits.class.getSimpleName();
     }
-    
-    
-    add
+
+    /**
+     * Returns the singleton instance of this class.
+     *
+     * @return the metric system instance.
+     */
+    public static AdditionalUnits getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * An angle unit accepted for use with SI units (standard name
+     * <code>ha</code>).
+     */
+    public static final Unit<Area> HECTARE = new TransformedUnit<Area>(SQUARE_METRE, new RationalConverter(10000, 1));
+
+    public static <U extends Unit<?>> U addUnit(U unit, String name, String symbol) {
+        //        if (isLabel) {
+        //            SimpleUnitFormat.getInstance().label(unit, text);
+        //        }
+        if (name != null && unit instanceof AbstractUnit) {
+            return Helper.addUnit(INSTANCE.units, unit, name);
+        } else {
+            INSTANCE.units.add(unit);
+        }
+        return unit;
+    }
 
 }
