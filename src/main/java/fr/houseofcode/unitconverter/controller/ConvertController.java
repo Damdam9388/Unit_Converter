a123248ea022435dd96028b8c98b1100ffe594ed
mre <tubingen050923>
2020-01-21 13:03:39 +0100

    #2 refactoring controller

--- src/main/java/fr/houseofcode/unitconverter/controller/ConvertController.java
+++ src/main/java/fr/houseofcode/unitconverter/controller/ConvertController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import fr.houseofcode.unitconverter.service.UnitConverterService;
 
@@ -22,11 +20,13 @@ public class ConvertController {
     @Autowired
     private UnitConverterService unitConverterService;
 
-    @RequestMapping("/convert")
+    @GetMapping("/convert")
+    public String unitForm(){
+        return "Welcome";
+    }
+
+    @PostMapping("/convert")
     public String unitConverter(Model model, @Valid @ModelAttribute("data")InputContent data, BindingResult result) {
-        if(data == null && result.hasErrors()){
-            return "Welcome";
-        }
         Map<String, String> map = new HashMap<>();
         map.put("spring", "mvc");
         if (data.getValue() != null){
