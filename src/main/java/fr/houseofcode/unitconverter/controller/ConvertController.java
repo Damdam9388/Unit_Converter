e849312ed6afb3e593eb90b97dc61247f63e7646
mre <tubingen050923>
2020-01-21 15:44:44 +0100

    #2 adding exception handler

--- src/main/java/fr/houseofcode/unitconverter/controller/ConvertController.java
+++ src/main/java/fr/houseofcode/unitconverter/controller/ConvertController.java
@@ -3,14 +3,17 @@ package fr.houseofcode.unitconverter.controller;
 import fr.houseofcode.unitconverter.entity.InputContent;
 import fr.houseofcode.unitconverter.exceptions.unitException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.*;
 
 import fr.houseofcode.unitconverter.service.UnitConverterService;
 
 import javax.measure.UnitConverter;
 import javax.validation.Valid;
+import java.sql.SQLException;
 
 @Controller
 public class ConvertController {
@@ -18,6 +21,11 @@ public class ConvertController {
 
     public ConvertController(@Autowired UnitConverterService unitConverterService){
         this.unitConverterService = unitConverterService;
+    }
+
+    @ExceptionHandler({BindException.class})
+    public String valueException(){
+        return "BindException";
     }
 
     @GetMapping("/convert")
