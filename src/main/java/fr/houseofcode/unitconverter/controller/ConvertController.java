57ada79bd26c10d5d843cf6b895facbc5c0cbc00
mre <tubingen050923>
2020-01-23 09:40:50 +0100

    #3 refactoring code

--- src/main/java/fr/houseofcode/unitconverter/controller/ConvertController.java
+++ src/main/java/fr/houseofcode/unitconverter/controller/ConvertController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import fr.houseofcode.unitconverter.service.UnitConverterService;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 @Controller
 public class ConvertController {
@@ -39,6 +40,13 @@ public class ConvertController {
     @GetMapping("/convert")
     public String unitForm(Model model){
         InputContent res = new InputContent();
+
+//        List<String> allInData = new ArrayList<String>();
+//        allInData.add("M²");
+//        res.setAllInputData(allInData);
+
+
+
         model.addAttribute("result", res);
         return "Converter";
     }
@@ -47,28 +55,51 @@ public class ConvertController {
     public String unitConverter(Model model, @Valid @ModelAttribute("data")InputContent data, BindingResult bindingResult) {
         InputContent res2 = new InputContent();
         if(bindingResult.hasErrors()){
-            model.addAttribute("errorMessage", "Veuillez corriger les erreurs suivantes :");
-            model.addAttribute("bindingResult", bindingResult);
+            bindingResultError(model, bindingResult);
             return "Converter";
         } else if (data.getValue() > 0 && !bindingResult.hasErrors()){
-            Double res = null;
-            if (data.getInputState().equals("m2") && data.getOutputState().equals("hectare")) {
-                res = unitConverterService.meterToHectare(data.getValue());
-            } else if (data.getInputState().equals("Kw") && data.getOutputState().equals("Co2")) {
-                res = unitConverterService.kwatttoco2(data.getValue());
-            }
+            Double res = calculMethodToChoose(data);
 
             res2.setInputState(data.getInputState());
             res2.setOutputState(data.getOutputState());
             res2.setValue(res);
-            model.addAttribute("dataValue", data.getValue());
-            model.addAttribute("inputUnit", data.getInputState());
-            model.addAttribute("outputUnit", data.getOutputState());
-            model.addAttribute("result", res2);
-        }else if(data.getValue() < 0){
-            model.addAttribute("negativeNumber", "you cannot insert negative number");
+
+            addAttributeToModel(model, data, res2);
+        }else if(data.getValue() < 0 && !bindingResult.hasErrors()){
+            negativeNumberError(model);
         }
 
         return "Converter";
+    }
+
+    private Double calculMethodToChoose(InputContent data){
+        Double res = null;
+        if (data.getInputState().equals("m2") && data.getOutputState().equals("hectare")) {
+            res = unitConverterService.meterToHectare(data.getValue());
+        } else if (data.getInputState().equals("Kw") && data.getOutputState().equals("Co2")) {
+            res = unitConverterService.kwatttoco2(data.getValue());
+        } else if (data.getInputState().equals("hectare") && data.getOutputState().equals("m2")) {
+            res = unitConverterService.hectareToMeter(data.getValue());
+        } else if (data.getInputState().equals("Co2") && data.getOutputState().equals("Kw")) {
+            res = unitConverterService.co2ToKwatt(data.getValue());
+        }
+
+        return res;
+    }
+
+    private void negativeNumberError(Model model){
+        model.addAttribute("negativeNumber", "you cannot insert negative number");
+    }
+
+    private void bindingResultError(Model model, BindingResult bindingResult){
+        model.addAttribute("errorMessage", "Veuillez corriger les erreurs suivantes :");
+        model.addAttribute("bindingResult", bindingResult);
+    }
+
+    private void addAttributeToModel(Model model, InputContent data, InputContent res2){
+        model.addAttribute("dataValue", data.getValue());
+        model.addAttribute("inputUnit", data.getInputState());
+        model.addAttribute("outputUnit", data.getOutputState());
+        model.addAttribute("result", res2);
     }
 }
--- src/main/java/fr/houseofcode/unitconverter/service/UnitConverterService.java
+++ src/main/java/fr/houseofcode/unitconverter/service/UnitConverterService.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package fr.houseofcode.unitconverter.service;
 
 import javax.measure.UnitConverter;
@@ -19,30 +16,27 @@ public class UnitConverterService {
 
     public double meterToHectare(double value) {
         UnitConverter m2 = SI.SQUARE_METRE.getConverterTo(AdditionalUnits.HECTARE);
-        double value1 = m2.convert(value);
-        return value1;
+        return m2.convert(value);
     }
 
     public double hectareToMeter(double value) {
         UnitConverter ha = AdditionalUnits.HECTARE.getConverterTo(SI.SQUARE_METRE);
-        double value2 = ha.convert(value);
-        return value2;
+        return ha.convert(value);
     }
 
     public double watttokwatt(double value) {
         UnitConverter kwatt = SI.WATT.getConverterTo(AdditionalUnits.KWATT);
-        double value1 = kwatt.convert(value);
-        return value1;
+        return kwatt.convert(value);
     }
 
     public double kwatttowatt(double value) {
         UnitConverter watt = AdditionalUnits.KWATT.getConverterTo(AdditionalUnits.WATT);
-        double value1 = watt.convert(value);
-        return value1;
+        return watt.convert(value);
     }
 
     public double kwatttoco2(double value) {
         return value * 0.09;
     }
+    public double co2ToKwatt(double value) {return value / 0.09; }
 
 }
